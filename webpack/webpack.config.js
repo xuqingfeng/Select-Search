02b396d79f7b1de72f19543ca0f4f36be6227683
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-07-21 17:45:55 +0800

    3.0.0

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
    },
    plugins: [
       new CopyPlugin({
-         patterns: [{from: ".", to: ".", context: "public"}]
+         patterns: [{ from: ".", to: ".", context: "public" }]
       }),
    ],
 };
