573577cb7a4234b1779accf4b3fca19a6a5f0ecf
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-07-21 13:56:15 +0800

    Replace gulp with webpack

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="chrome/chrome.d.ts" />
-/// <reference path="filesystem/filesystem.d.ts" />
-/// <reference path="filewriter/filewriter.d.ts" />
-/// <reference path="webrtc/MediaStream.d.ts" />
