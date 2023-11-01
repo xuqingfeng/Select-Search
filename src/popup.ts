0bdf64733da985bac413c495031f68209b2d08d5
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-11-01 11:31:45 +0800

    Load key from chrome storage

--- src/popup.ts
+++ src/popup.ts
@@ -5,3 +5,51 @@ document.querySelector("#go-to-options").addEventListener("click", function () {
     window.open(chrome.runtime.getURL("options.html"));
   }
 });
+
+class Popup {
+  static keyMap = [
+    "searchKey",
+    "translateKey",
+    "jumpToLinkKey",
+  ];
+
+  constructor(
+    public searchKey = "g",
+    public translateKey = "e",
+    public jumpToLinkKey = "b"
+  ) {
+    for (let key of Popup.keyMap) {
+      this.chromeGet(key);
+    }
+  }
+
+  chromeGet = (key: string) => {
+    let self = this;
+    chrome.storage.sync.get(key, function (items) {
+      let keyValue: string;
+      switch (key) {
+        case "searchKey":
+          keyValue = items[key] || "g";
+          self.setText(key, keyValue.toUpperCase());
+          break;
+        case "translateKey":
+          keyValue = items[key] || "e";
+          self.setText(key, keyValue.toUpperCase());
+          break;
+        case "jumpToLinkKey":
+          keyValue = items[key] || "b";
+          self.setText(key, keyValue.toUpperCase());
+          break;
+        default:
+        //
+      }
+    });
+  };
+
+  setText(key: string, value: string) {
+    let ele = document.getElementById(key) as HTMLElement;
+    ele.textContent = value;
+  }
+}
+
+let popup = new Popup();
